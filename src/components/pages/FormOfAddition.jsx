c52dc58d3b583dc786de04fe96f9dcffe4f1f74b
Al101020 <alex.vl.m@yandex.ru>
2026-03-19 22:29:15 +1000

    commit-0.07d

--- src/components/pages/FormOfAddition.jsx
+++ src/components/pages/FormOfAddition.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import ShowServices from './ShowServices';
+// import ShowServices from './FormOfAddition/ShowServices';
+import ShowServices from './FormOfAddition/ShowServices';
 
 export default function FormOfAddition(props) {
 
--- src/components/pages/FormOfAddition/ShowServices.jsx
+++ src/components/pages/FormOfAddition/ShowServices.jsx
@@ -6,7 +6,16 @@ export default function ShowServices(props) {  // console.log(props);
     // console.log('длина массива = 0');
     return;
   }
-  console.log(arrServis);
+  // console.log(arrServis);
+
+  const btnEdit = () => {
+    console.log(' кнопка btnEdit')
+  }
+
+  const btnDel = () => {
+    console.log(' кнопка btnDel')
+  }
+
   return (
     <div className='ShowServices'>
       {arrServis.map(service => (
@@ -14,6 +23,8 @@ export default function ShowServices(props) {  // console.log(props);
           <div className='marker'></div>
           <div className='serviceName'>{service.name}</div>
           <div className='servicePrice'>{service.price}</div>
+          <button className='btnEdit' onClick={btnEdit}>✎</button>
+          <button className='btnDel' onClick={btnDel}>X</button>
         </div>
       ))}
     </div>
